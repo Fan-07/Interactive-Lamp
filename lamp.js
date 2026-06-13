bef86fcc8734774ce84ed393b734bbc659f035d8
Fawn <kuikelprabhav@gmail.com>
2026-06-13 22:39:20 +0545

    Update lamp.js

--- lamp.js
+++ lamp.js
@@ -1,16 +1,7 @@
 const lamp = document.getElementById("lamp");
 const body = document.body;
 
-let isOn = false;
-
 lamp.addEventListener("click", () => {
-    isOn = !isOn;
-
-    if (isOn) {
-        body.classList.add("light-on");
-        lamp.textContent = "💡";
-    } else {
-        body.classList.remove("light-on");
-        lamp.textContent = "💡";
-    }
+    body.classList.toggle("dark");
+    body.classList.toggle("light-on");
 });
